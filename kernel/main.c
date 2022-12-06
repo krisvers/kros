cb22b27dd6b2f4072cd8bbc5645edbd4db806d93
Nicholas Kelly - Student <kellynicholasi@lexington1.net>
2022-12-06 23:16:19 +0000

    reworking the font

--- kernel/main.c
+++ kernel/main.c
@@ -1,8 +1,5 @@
 #include <video/font.h>
 
 void main() {
-	text_putc(0, 0, '!', RED);
-	text_putc(5, 0, '!'+1, ORANGE);
-	text_putc(10, 0, '!'+2, YELLOW);
-	text_putc(15, 0, '!'+3, GREEN);
+	text_putc(0, 0, '!', YELLOW);
 }
