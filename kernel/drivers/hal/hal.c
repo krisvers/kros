85a6bcaac59f20d4c04bef6e23d33474ae7df386
krisvers <nakoidevn@gmail.com>
2022-12-15 22:36:39 -0500

    annoying space tabs in hal.c

--- kernel/drivers/hal/hal.c
+++ kernel/drivers/hal/hal.c
@@ -2,11 +2,11 @@
 
 void hal_init() {
 	GDT g_GDT = {
-		{ 0x0000, 0, 0, 0x00, 0x00, 0 },        //    NULL
-		{ 0xFFFF, 0, 0, 0x9A, 0xAF, 0 },        //    Kernel Code
-    	{ 0xFFFF, 0, 0, 0x92, 0xCF, 0 },        //    Kernel Data
-    	{ 0xFFFF, 0, 0, 0xFA, 0xAF, 0 },        //    User Code
-    	{ 0xFFFF, 0, 0, 0xF2, 0xCF, 0 }        //    User Data
+		{ 0x0000, 0, 0, 0x00, 0x00, 0 },		//    NULL
+		{ 0xFFFF, 0, 0, 0x9A, 0xAF, 0 },		//    Kernel Code
+    	{ 0xFFFF, 0, 0, 0x92, 0xCF, 0 },		//    Kernel Data
+    	{ 0xFFFF, 0, 0, 0xFA, 0xAF, 0 },		//    User Code
+    	{ 0xFFFF, 0, 0, 0xF2, 0xCF, 0 }			//    User Data
 	};
 
 	GDTDesc g_GDTR = { sizeof(g_GDT) - 1, &g_GDT };
